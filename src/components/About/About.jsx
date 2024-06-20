ead7acf876d89b23f1aa4757937007940394e741
Andrés Marín <andresfprivado@gmail.com>
2024-06-21 00:44:39 +0200

    Portfolio update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,6 +8,11 @@ import tailwindIcon from '../../assets/img/icons/tailwind.svg';
 import javaIcon from '../../assets/img/icons/java.svg';
 import nodejsIcon from '../../assets/img/icons/nodejs.svg';
 import sqlIcon from '../../assets/img/icons/sql.svg';
+import graphqlIcon from '../../assets/img/icons/graphql.svg';
+import reduxIcon from '../../assets/img/icons/redux.svg';
+import typescriptIcon from '../../assets/img/icons/typescript.svg';
+import nextjsIcon from '../../assets/img/icons/nextjs.svg';
+import mongodbIcon from '../../assets/img/icons/mongodb.svg';
 
 const About = () => {
 
@@ -31,69 +36,103 @@ const About = () => {
     }); 
 
     return (
-        <div className='portfolio-about section'>
-            <div className="about-container">
-                <div className="about-main">
-                    <div className="about-main-title">
-                        <h2>Hello!</h2>
-                        <div className="main-title-ellipse"></div>
-                    </div>
-                    <p>I’m autodidact Web Developer located in Spain, passionate about design, challenges and programming. Since I discovered that I can combine all of this in web development, it has become my true passion.</p>
-                    <p>I am aiming to be a Full-Stack Developer, since I want to domain as many technologies as possible.</p>
-                </div>
-                <div className="about-sections">
-                    <div className="about-education">
-                        <h2>Education</h2>
-                        <div className="education">
-                            <span>September 2022 - Present</span>
-                            <p>Web Application Development | AFA Formación</p>
-                        </div>
-                    </div>
-                    <div className="about-skills">
-                        <h2>Skills</h2>
-                        <div className="skills">
-                            <ul className="skills-list">
-                                <li className="skill">
-                                    <img src={reactIcon}/>
-                                    <span>React</span>
-                                </li>
-                                <li className="skill">
-                                    <img src={jsIcon}/>
-                                    <span>JavaScript</span>
-                                </li>
-                                <li className="skill">
-                                    <img src={sassIcon}/>
-                                    <span>SASS</span>
-                                </li>
-                                <li className="skill">
-                                    <img src={gsapIcon}/>
-                                    <span>GSAP</span>
-                                </li>
-                                <li className="skill">
-                                    <img src={tailwindIcon}/>
-                                    <span>Tailwind</span>
-                                </li>
-                            </ul>
-                            <ul className="skills-list">
-                                <li className="skill">
-                                    <img src={javaIcon}/>
-                                    <span>Java</span>
-                                </li>
-                                <li className="skill">
-                                    <img src={nodejsIcon}/>
-                                    <span>NodeJS</span>
-                                </li>
-                                <li className="skill">
-                                    <img src={sqlIcon}/>
-                                    <span>MySQL</span>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
+      <div className="portfolio-about section">
+        <div className="about-container">
+          <div className="about-main">
+            <div className="about-main-title">
+              <h2>Hello!</h2>
+              <div className="main-title-ellipse"></div>
             </div>
+            <p>
+              I’m autodidact Web Developer located in Spain, passionate about
+              design, challenges and programming. Since I discovered that I can
+              combine all of this in web development, it has become my true
+              passion.
+            </p>
+            <p>
+              I am aiming to be a Full-Stack Developer, since I want to domain
+              as many technologies as possible.
+            </p>
+          </div>
+          <div className="about-sections">
+            <div className="about-education">
+              <h2>Education</h2>
+              <div className="education">
+                <span>September 2022 - Present</span>
+                <p>Web Application Development | AFA Formación / Unendo</p>
+              </div>
+              <div className="education">
+                <span>January 2020 - Present</span>
+                <p>Web Application Development | Self-study</p>
+                <p className='subtitle'>Continuous learning on my own.</p>
+              </div>
+            </div>
+            <div className="about-skills">
+              <h2>Skills</h2>
+              <div className="skills">
+                <ul className="skills-list">
+                  <li className="skill">
+                    <img src={nextjsIcon} />
+                    <span>NextJS</span>
+                  </li>
+                  <li className="skill">
+                    <img src={reactIcon} />
+                    <span>React</span>
+                  </li>
+                  <li className="skill">
+                    <img src={jsIcon} />
+                    <span>JavaScript</span>
+                  </li>
+                  <li className="skill">
+                    <img src={sassIcon} />
+                    <span>SASS</span>
+                  </li>
+                  <li className="skill">
+                    <img src={typescriptIcon} />
+                    <span>TypeScript</span>
+                  </li>
+                  <li className="skill">
+                    <img src={tailwindIcon} />
+                    <span>Tailwind</span>
+                  </li>
+                </ul>
+                <ul className="skills-list">
+                  <li className="skill">
+                    <img src={javaIcon} />
+                    <span>Java</span>
+                  </li>
+                  <li className="skill">
+                    <img src={nodejsIcon} />
+                    <span>NodeJS</span>
+                  </li>
+                  <li className="skill">
+                    <img src={sqlIcon} />
+                    <span>MySQL</span>
+                  </li>
+                  <li className="skill">
+                    <img src={mongodbIcon} />
+                    <span>MongoDB</span>
+                  </li>
+                  <li className="skill">
+                    <img src={reduxIcon} />
+                    <span>Redux</span>
+                  </li>
+                  <li className="skill">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="white"
+                      viewBox="0 0 16 16">
+                      <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3" />
+                    </svg>
+                    <span>More</span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    );
 }
 
 export default About
--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -3,11 +3,16 @@ import React, { useEffect, useRef } from 'react'
 import work1 from '../../assets/img/work/work_1.png';
 import work2 from '../../assets/img/work/work_2.png';
 import work3 from '../../assets/img/work/work_3.png';
+import work4 from '../../assets/img/work/work_4.png';
 import sassIcon from '../../assets/img/icons/sass.svg';
 import jsIcon from '../../assets/img/icons/js.svg';
 import reactIcon from '../../assets/img/icons/react.svg';
 import graphqlIcon from '../../assets/img/icons/graphql.svg';
 import reduxIcon from '../../assets/img/icons/redux.svg';
+import typescriptIcon from '../../assets/img/icons/typescript.svg';
+import nextjsIcon from '../../assets/img/icons/nextjs.svg';
+import mongodbIcon from '../../assets/img/icons/mongodb.svg';
+import tailwindIcon from '../../assets/img/icons/tailwind.svg';
 
 import { gsap } from "gsap";
 
@@ -47,6 +52,65 @@ const Work = () => {
     return (
     <div className='portfolio-work section'>
         <div ref={works} className="works-container">
+        <div className="work" style={{ backgroundImage: "url(" + work4 + ")" }}>
+                <a className="work-link" href="https://marins-japanese-restaurant.netlify.app/" target="_blank">
+                    <div className="work-container">
+                        <span className='work-title'>Marín's Asian Store</span>
+                        <ul className="work-tech">
+                            <li className='tech-icon'>
+                                <img src={nextjsIcon}/>
+                            </li>
+                            <li className='tech-icon'>
+                                <img src={typescriptIcon}/>
+                            </li>
+                            <li className='tech-icon'>
+                                <img src={tailwindIcon}/>
+                            </li>
+                            <li className='tech-icon'>
+                                {<img src={reactIcon}/>}
+                            </li>
+                            <li className='tech-icon'>
+                                {<img src={reduxIcon}/>}
+                            </li>
+                            <li className='tech-icon'>
+                                {<img src={sassIcon}/>}
+                            </li>
+                            <li className='tech-icon'>
+                                {<img src={mongodbIcon}/>}
+                            </li>
+                        </ul>
+                    </div>
+                </a>
+                <div className="work-preview">
+                    <a className="work-link" href="https://marins-japanese-restaurant.netlify.app/" target="_blank">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="work-link-icon" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0v-6z"/>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+            <div className="work" style={{ backgroundImage: "url(" + work2 + ")" }}>
+                <a className="work-link" href="https://marins-japanese-restaurant.netlify.app/" target="_blank">
+                    <div className="work-container">
+                        <span className='work-title'>Marín's Japanese Restaurant</span>
+                        <ul className="work-tech">
+                            <li className='tech-icon'>
+                                <img src={jsIcon}/>
+                            </li>
+                            <li className='tech-icon'>
+                                {<img src={sassIcon}/>}
+                            </li>
+                        </ul>
+                    </div>
+                </a>
+                <div className="work-preview">
+                    <a className="work-link" href="https://marins-japanese-restaurant.netlify.app/" target="_blank">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="work-link-icon" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0v-6z"/>
+                        </svg>
+                    </a>
+                </div>
+            </div>
             <div className="work" style={{ backgroundImage: "url(" + work3 + ")" }}>
                 <a className="work-link"href="https://animes-app-am.netlify.app/" target="_blank">         
                     <div className="work-container">
@@ -97,34 +161,8 @@ const Work = () => {
                     </a>
                 </div>
             </div>
-            <div className="work" style={{ backgroundImage: "url(" + work2 + ")" }}>
-                <a className="work-link" href="https://marins-japanese-restaurant.netlify.app/" target="_blank">
-                    <div className="work-container">
-                        <span className='work-title'>Maríns Japanese Restaurant</span>
-                        <ul className="work-tech">
-                            <li className='tech-icon'>
-                                <img src={jsIcon}/>
-                            </li>
-                            <li className='tech-icon'>
-                                {<img src={sassIcon}/>}
-                            </li>
-                        </ul>
-                    </div>
-                </a>
-                <div className="work-preview">
-                    <a className="work-link" href="https://marins-japanese-restaurant.netlify.app/" target="_blank">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="work-link-icon" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0v-6z"/>
-                        </svg>
-                    </a>
-                </div>
-            </div>
-            <div className="work work-empty hidden lg:flex">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className='work-empty-icon' viewBox="0 0 16 16">
-                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                    <path d="M9.146 5.146a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708.708l-.647.646.647.646a.5.5 0 0 1-.708.708l-.646-.647-.646.647a.5.5 0 1 1-.708-.708l.647-.646-.647-.646a.5.5 0 0 1 0-.708zm-5 0a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 1 1 .708.708l-.647.646.647.646a.5.5 0 1 1-.708.708L5.5 7.207l-.646.647a.5.5 0 1 1-.708-.708l.647-.646-.647-.646a.5.5 0 0 1 0-.708zM10 11a2 2 0 1 1-4 0 2 2 0 0 1 4 0z"/>
-                </svg>
-            </div>
+
+
             <div className="work work-empty hidden xl:flex">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className='work-empty-icon' viewBox="0 0 16 16">
                     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
