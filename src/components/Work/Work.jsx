487e2b4078d70a969bc7534033eaf9aaede5c33b
Andrés Marín <andresfprivado@gmail.com>
2024-06-21 14:57:10 +0200

    Update link

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -82,7 +82,7 @@ const Work = () => {
                     </div>
                 </a>
                 <div className="work-preview">
-                    <a className="work-link" href="https://marins-japanese-restaurant.netlify.app/" target="_blank">
+                    <a className="work-link" href="https://marins-asian-store.vercel.app/" target="_blank">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="work-link-icon" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0v-6z"/>
                         </svg>
